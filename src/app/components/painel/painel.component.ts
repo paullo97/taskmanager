445ea3ce8144d8735d69e7c93918b08292917bbb
Paulo Silva <paulo.silva@celk.net>
2023-06-20 16:50:02 -0300

    inlcude functions and rules with snackbars and conditions

--- src/app/components/painel/painel.component.ts
+++ src/app/components/painel/painel.component.ts
@@ -3,6 +3,13 @@ import { MatDialog } from "@angular/material/dialog";
 import { DialogDeleteComponent } from "../dialog-delete/dialog-delete.component";
 import { DialogCompleteComponent } from "../dialog-complete/dialog-complete.component";
 import { DialogCreateComponent } from "../dialog-create/dialog-create.component";
+import { Store } from "@ngrx/store";
+import { TaskManagerStore } from "src/app/core/store/taskmanager/taskmanager.store";
+import { getListLength, getListTask, getLoading } from "src/app/core/store/taskmanager/taskmanager.selectors";
+import { Observable } from "rxjs";
+import { ITask } from "src/app/core/model/task.model";
+import { MatSnackBar } from "@angular/material/snack-bar";
+import { completeTasks, deleteTask, editTask } from "src/app/core/store/taskmanager/taskmanager.actions";
 
 @Component({
     selector: 'painel-app',
@@ -10,28 +17,72 @@ import { DialogCreateComponent } from "../dialog-create/dialog-create.component"
     styleUrls: ['./painel.component.scss']
 })
 export class PainelComponent {
-    public typesOfShoes: Array<string> = ['Boots', 'Clogs', 'Loafers', 'Moccasins'];
-    public itensSelecionados: Array<string> = [];
+    public loading$: Observable<boolean> = this.taskStore.select(getLoading);
+    public listTask$: Observable<Array<ITask>> = this.taskStore.select(getListTask);
+    public listLength$: Observable<number> = this.taskStore.select(getListLength);
+    
+    public itensSelecionados: Array<ITask> = [];
 
-    constructor(public dialog: MatDialog) { }
+    constructor(
+        public dialog: MatDialog,
+        private readonly taskStore: Store<TaskManagerStore>,
+        private _snackBar: MatSnackBar
 
-    public obterItensSelecionados(): void {
-        console.log('Itens Selecionados:', this.itensSelecionados);
-    }
+    )
+    { }
 
     public deleteTasks(): void {
-        this.dialog.open(DialogDeleteComponent);
+        if(this.itensSelecionados.length === 0)
+        {
+            this._snackBar.open('É preciso ao menos ter uma Task para realizar o Delete', 'OK', { duration: 2000 });
+            return;
+        }
+
+        this.dialog.open(DialogDeleteComponent).afterClosed().subscribe((data) =>
+        {
+            if(data)
+            {
+                this.taskStore.dispatch(deleteTask({ tasks: this.itensSelecionados }));
+                this._snackBar.open('Task Deletadas Com sucesso!', 'OK', { duration: 2000 });
+            }
+        })
     }
 
     public completeTasks(): void {
-        this.dialog.open(DialogCompleteComponent);
-    }
+        if(this.itensSelecionados.length === 0)
+        {
+            this._snackBar.open('É preciso ao menos ter uma Task selecionada para Finaliza-las', 'OK', {duration: 2000});
+            return;
+        }
 
-    public editCreate(args: string = ''): void {
-        this.dialog.open(DialogCreateComponent, {
-            data: {
-                id: args
+        this.dialog.open(DialogCompleteComponent).afterClosed().subscribe((data) =>
+        {
+            if(data)
+            {
+                this.taskStore.dispatch(completeTasks({
+                    tasks: this.itensSelecionados
+                }));
             }
         });
+    }
+
+    public createTask(): void
+    {
+        this.dialog.open(DialogCreateComponent);
+    }
+
+    public editTask(): void
+    {
+        if(this.itensSelecionados.length != 1)
+        {
+            this._snackBar.open('Para Editar uma tarefa, precisa ter somente uma Task Selecionada', 'OK', {duration: 2000})
+            return;
+        }
+
+        this.taskStore.dispatch(editTask({
+            task: this.itensSelecionados[0]
+        }));
+
+        this.dialog.open(DialogCreateComponent);
     }
 }
